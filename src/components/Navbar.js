89a2a7bfbe06b1f672279db14b3637ca57bf4dd6
loserchik67 <loserchik67@gmail.com>
2023-03-19 12:25:10 -0500

    Navbar Created

---
+++ src/components/Navbar.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import { Button } from "./Button";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import { useEffect } from "react";
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener("resize", showButton);
+
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            Loserchik67 <i className="fa-solid fa-mountain-sun"> </i>
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/Games" className="nav-links" onClick={closeMobileMenu}>
+                Games
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/Projectss"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Projects
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/Sample Website"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Sample Website
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/Contact"
+                className="nav-links-mobile"
+                onClick={closeMobileMenu}
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+          {button && <Button buttonStyle="btn--outline"> Contact </Button>}
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
