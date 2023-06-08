02ae5130a38aa1beb76923e4ef34929748b5d3ab
loserchik67 <loserchik67@gmail.com>
2023-06-07 23:05:33 -0500

    updated images

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -43,7 +43,7 @@ function Navbar() {
             </li>
             <li className="nav-item">
               <Link
-                to="/Projectss"
+                to="/Projects"
                 className="nav-links"
                 onClick={closeMobileMenu}
               >
